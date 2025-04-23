bd104b156639f3a7a5ff827915a3a309c4ebcb0f
RodrigoTrevmore <sosimousa@gmail.com>
2025-04-23 00:56:07 -0400

    second  commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -13,24 +13,31 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
+
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem ({ name: item.name, }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
   };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+   };
 
   return (
     <div className="cart-container">
